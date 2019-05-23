46a01313659ae528a427b026be0f8722c92ea381
rileyeh <rileyehatch@gmail.com>
2019-05-23 17:07:46 -0600

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
+
+let baseUrl = 'https://practiceapi.devmountain.com/api'
 
 class App extends Component {
   constructor() {
@@ -19,19 +23,52 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    // axios.get(`${baseUrl}/posts`)
+    // .then(response => {
+    //   this.setState({
+    //     posts: response.data
+    //   })
+    // })
+    // .catch(err => {
+    //   console.log(err)
+    // })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`${baseUrl}/posts?id=${ id }`, {text})
+    .then(response => {
+      this.setState({
+        posts: response.data
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
-  deletePost() {
+  deletePost(id) {
+    axios.delete(`${baseUrl}/posts?id=${ id }`)
+    .then(response => {
+      this.setState({
+        posts: response.data
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    }
 
-  }
 
-  createPost() {
-
+  createPost(id, text) {
+    axios.post(`${baseUrl}/posts`, {text}) 
+    .then(response => {
+      this.setState({
+        posts: response.data
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
   render() {
@@ -43,7 +80,19 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={ this.createPost } />
+
+          {
+            posts.map(post => (
+              <Post key={ post.id }
+                    text={ post.text }
+                    date={ post.date } 
+                    updatePostFn={ this.updatePost } 
+                    id={ post.id }
+                    deletePostFn={ this.deletePost }
+                    />
+          ))
+        }
           
         </section>
       </div>
